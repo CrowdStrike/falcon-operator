9e449852a3c1d651b31e1b77957eb25632385a7c
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-24 15:39:18 +0100

    Allow the operator to manage image streams

--- controllers/falcon/falconconfig_controller.go
+++ controllers/falcon/falconconfig_controller.go
@@ -29,7 +29,7 @@ type FalconConfigReconciler struct {
 // +kubebuilder:rbac:groups=falcon.crowdstrike.com,resources=falconconfigs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=falcon.crowdstrike.com,resources=falconconfigs/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=falcon.crowdstrike.com,resources=falconconfigs/finalizers,verbs=update
-// +kubebuilder:rbac:groups=image.openshift.io,resources=imagestreams,verbs=get,create,update,delete
+// +kubebuilder:rbac:groups=image.openshift.io,resources=imagestreams,verbs=get;create;update;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
