e0fd4d432ab427ba25786b7ae07fbd0b49bd0708
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-25 15:48:35 +0100

    Refactor: move ImageStream creation to the pending phase

--- controllers/falcon/falconconfig_controller.go
+++ controllers/falcon/falconconfig_controller.go
@@ -10,13 +10,9 @@ import (
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	types "k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	imagev1 "github.com/openshift/api/image/v1"
 
 	falconv1alpha1 "github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1"
 	"github.com/crowdstrike/falcon-operator/pkg/falcon_container"
@@ -72,29 +68,6 @@ func (r *FalconConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	switch instanceToBeUpdated.Status.Phase {
 	case falconv1alpha1.PhasePending:
 		return r.phasePendingReconcile(ctx, instanceToBeUpdated, logger)
-	}
-
-	imageStream := imagev1.ImageStream{}
-	err = r.Client.Get(ctx, types.NamespacedName{Name: "falcon-container", Namespace: req.NamespacedName.Namespace}, &imageStream)
-	if err != nil && errors.IsNotFound(err) {
-		imageStream := &imagev1.ImageStream{
-			TypeMeta:   metav1.TypeMeta{APIVersion: imagev1.SchemeGroupVersion.String(), Kind: "ImageStream"},
-			ObjectMeta: metav1.ObjectMeta{Name: "falcon-container", Namespace: req.NamespacedName.Namespace},
-			Spec:       imagev1.ImageStreamSpec{},
-		}
-		logger.Info("Creating a new ImageStream", "ImageStream.Namespace", imageStream.Namespace, "ImageStream.Name", imageStream.Name)
-		err = r.Client.Create(ctx, imageStream)
-		if err != nil {
-			if !errors.IsAlreadyExists(err) {
-				logger.Error(err, "Failed to create new ImageStream", "ImageStream.Namespace", imageStream.Namespace, "ImageStream.Name", imageStream.Name)
-				return ctrl.Result{}, err
-			}
-		}
-		return ctrl.Result{Requeue: true}, nil
-
-	} else if err != nil {
-		logger.Error(err, "Failed to get ImageStream")
-		return ctrl.Result{}, err
 	}
 
 	refreshImage, err := r.reconcileContainerImage(falconConfig)
