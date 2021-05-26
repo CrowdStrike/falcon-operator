f9f82d51f129e24a88cc45482e4486dfee50c252
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-26 18:00:00 +0100

    Refactor: Extract method: imageStream

--- controllers/falcon/phase_pending.go
+++ controllers/falcon/phase_pending.go
@@ -21,8 +21,7 @@ const (
 func (r *FalconConfigReconciler) phasePendingReconcile(ctx context.Context, instance *falconv1alpha1.FalconConfig, logger logr.Logger) (ctrl.Result, error) {
 	logger.Info("Phase: Pending")
 
-	imageStream := imagev1.ImageStream{}
-	err := r.Client.Get(ctx, types.NamespacedName{Name: IMAGE_STREAM_NAME, Namespace: instance.ObjectMeta.Namespace}, &imageStream)
+	_, err := r.imageStream(ctx, instance.ObjectMeta.Namespace)
 	if err != nil && errors.IsNotFound(err) {
 		imageStream := &imagev1.ImageStream{
 			TypeMeta:   metav1.TypeMeta{APIVersion: imagev1.SchemeGroupVersion.String(), Kind: "ImageStream"},
@@ -48,4 +47,9 @@ func (r *FalconConfigReconciler) phasePendingReconcile(ctx context.Context, inst
 
 	err = r.Client.Status().Update(ctx, instance)
 	return ctrl.Result{}, err
+}
+
+func (r *FalconConfigReconciler) imageStream(ctx context.Context, namespace string) (stream imagev1.ImageStream, err error) {
+	err = r.Client.Get(ctx, types.NamespacedName{Name: IMAGE_STREAM_NAME, Namespace: namespace}, &stream)
+	return
 }
