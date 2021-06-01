2c4310150aba0f811087db11913848036ff9aaae
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-06-01 16:37:24 +0100

    Deploy falcon injector to the cluster as kube.Job

--- controllers/falcon/phase_configuring.go
+++ controllers/falcon/phase_configuring.go
@@ -2,6 +2,7 @@ package falcon
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -11,6 +12,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	falconv1alpha1 "github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1"
 )
@@ -88,6 +90,17 @@ func (r *FalconConfigReconciler) phaseConfiguringReconcile(ctx context.Context, 
 
 	// (Step 3) verify configuration || or re-configure job
 	// (Step 4) wait for completion
+	if !isJobReady(job) {
+		logger.Info("Waiting for Job completion")
+		return ctrl.Result{RequeueAfter: time.Second * 5}, nil
+	}
+
+	pod, err := r.configurePod(ctx, instance, job, logger)
+	if err != nil {
+		return r.error(ctx, instance, "Failed to get pod relevant to configure job", err)
+	}
+	_ = pod
+
 	// (Step 5) obtain job output
 	logger.Error(nil, "TODO")
 
@@ -96,4 +109,29 @@ func (r *FalconConfigReconciler) phaseConfiguringReconcile(ctx context.Context, 
 
 	err = r.Client.Status().Update(ctx, instance)
 	return ctrl.Result{}, err
+}
+
+func (r *FalconConfigReconciler) configurePod(ctx context.Context, instance *falconv1alpha1.FalconConfig, job *batchv1.Job, logger logr.Logger) (*corev1.Pod, error) {
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(instance.ObjectMeta.Namespace),
+		client.MatchingLabels(map[string]string{"job-name": JOB_NAME}),
+	}
+	if err := r.List(ctx, podList, listOpts...); err != nil {
+		logger.Error(err, "Failed to list pods", "FalconConfig.Namespace", instance.ObjectMeta.Namespace, "FalconConfig.Name", instance.ObjectMeta.Name)
+		return nil, err
+	}
+	if len(podList.Items) != 1 {
+		return nil, fmt.Errorf("Found %d relevant pods, expected 1 pod", len(podList.Items))
+	}
+	return &podList.Items[0], nil
+}
+
+func isJobReady(job *batchv1.Job) bool {
+	for _, cond := range job.Status.Conditions {
+		if cond.Type == batchv1.JobComplete && cond.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
 }
