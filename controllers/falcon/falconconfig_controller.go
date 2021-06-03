1b116ffa1fde4103fded52cf23518ce9b6f59fc8
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-06-03 12:37:38 +0100

    Allow operator to create secrets
    It needs to create falcon-system/injector-tls

--- controllers/falcon/falconconfig_controller.go
+++ controllers/falcon/falconconfig_controller.go
@@ -35,6 +35,7 @@ type FalconConfigReconciler struct {
 // +kubebuilder:rbac:groups="",resources=pods/log,verbs=get
 
 // +kubebuilder:rbac:groups="",resources=namespaces,verbs=create
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=create
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
