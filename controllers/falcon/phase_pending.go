97f5f91f5eaaae1907bd9edcde299107be7c7ffd
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-06-04 14:20:12 +0100

    Refactor: Extract method: CreateImageStream

--- controllers/falcon/phase_pending.go
+++ controllers/falcon/phase_pending.go
@@ -6,17 +6,10 @@ import (
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
-
-	imagev1 "github.com/openshift/api/image/v1"
 
 	falconv1alpha1 "github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1"
 	"github.com/crowdstrike/falcon-operator/pkg/falcon_container_deployer"
-)
-
-const (
-	IMAGE_STREAM_NAME = "falcon-container"
 )
 
 func (r *FalconConfigReconciler) phasePendingReconcile(ctx context.Context, instance *falconv1alpha1.FalconConfig, logger logr.Logger) (ctrl.Result, error) {
@@ -30,19 +23,10 @@ func (r *FalconConfigReconciler) phasePendingReconcile(ctx context.Context, inst
 
 	_, err := d.GetImageStream()
 	if err != nil && errors.IsNotFound(err) {
-		imageStream := &imagev1.ImageStream{
-			TypeMeta:   metav1.TypeMeta{APIVersion: imagev1.SchemeGroupVersion.String(), Kind: "ImageStream"},
-			ObjectMeta: metav1.ObjectMeta{Name: IMAGE_STREAM_NAME, Namespace: d.Namespace()},
-			Spec:       imagev1.ImageStreamSpec{},
-		}
-		err = r.Client.Create(ctx, imageStream)
+		err := d.CreateImageStream()
 		if err != nil {
-			if !errors.IsAlreadyExists(err) {
-				logger.Error(err, "Failed to create new ImageStream", "ImageStream.Namespace", imageStream.Namespace, "ImageStream.Name", imageStream.Name)
-				return ctrl.Result{}, err
-			}
+			return d.Error("failed to create Image Stream", err)
 		}
-		logger.Info("Created a new ImageStream", "ImageStream.Namespace", d.Namespace(), "ImageStream.Name", imageStream.Name)
 		// It takes few moment for the ImageStream to be ready
 		return ctrl.Result{RequeueAfter: time.Second * 5}, nil
 
