b3b4c79047ec62327637ea277f42b4218a66756a
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-08-10 16:25:09 +0100

    Provide method to fetch AWS Region

--- pkg/aws/region.go
+++ pkg/aws/region.go
@@ -20,3 +20,19 @@ func AvailabilityZone() (string, error) {
 	body, err := ioutil.ReadAll(resp.Body)
 	return string(body), err
 }
+
+func Region() (string, error) {
+	// curl -s http://169.254.169.254/latest/meta-data/placement/region
+	req, err := http.NewRequest("GET", "http://169.254.169.254/latest/meta-data/placement/region", nil)
+	if err != nil {
+		return "", err
+	}
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	return string(body), err
+}
