1dcd6b33b69692bb6d8b3d694ba9443dd85fa883
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-08-10 16:22:15 +0100

    Provide method to fetch AWS Availability Zone

---
+++ pkg/aws/region.go
@@ -0,0 +1,22 @@
+package aws
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func AvailabilityZone() (string, error) {
+	// curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone
+	req, err := http.NewRequest("GET", "http://169.254.169.254/latest/meta-data/placement/availability-zone", nil)
+	if err != nil {
+		return "", err
+	}
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	return string(body), err
+}
