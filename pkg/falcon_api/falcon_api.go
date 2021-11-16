2118d04b0397e2629f74f7a4b4a3d4a7d354e00b
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-11-16 10:57:15 +0000

    Introduce method for fetching CCID from Falcon API

--- pkg/falcon_api/falcon_api.go
+++ pkg/falcon_api/falcon_api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/crowdstrike/gofalcon/falcon"
 	"github.com/crowdstrike/gofalcon/falcon/client/falcon_container"
+	"github.com/crowdstrike/gofalcon/falcon/client/sensor_download"
 )
 
 func RegistryToken(apiCfg *falcon.ApiConfig) (string, error) {
@@ -34,4 +35,26 @@ func RegistryToken(apiCfg *falcon.ApiConfig) (string, error) {
 	}
 	valueString := value.(string)
 	return valueString, nil
+}
+
+func CCID(apiCfg *falcon.ApiConfig) (string, error) {
+	client, err := falcon.NewClient(apiCfg)
+	if err != nil {
+		return "", err
+	}
+	response, err := client.SensorDownload.GetSensorInstallersCCIDByQuery(&sensor_download.GetSensorInstallersCCIDByQueryParams{
+		Context: apiCfg.Context,
+	})
+	if err != nil {
+		return "", fmt.Errorf("Could not get CCID from CrowdStrike Falcon API: %v", err)
+	}
+	payload := response.GetPayload()
+	if err = falcon.AssertNoError(payload.Errors); err != nil {
+		return "", fmt.Errorf("Error reported when getting CCID from CrowdStrike Falcon API: %v", err)
+	}
+	if len(payload.Resources) != 1 {
+		return "", fmt.Errorf("Unexpected API response: %v", payload.Resources)
+	}
+	return payload.Resources[0], nil
+
 }
