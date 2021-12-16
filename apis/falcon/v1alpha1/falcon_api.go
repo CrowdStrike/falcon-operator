e7fe4307f7acfb0a02029c79dd987c7f251ef5bb
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-12-16 13:51:46 +0100

    Simplify Falcon Cloud manipulation enabled by the latest gofalcon

--- apis/falcon/v1alpha1/falcon_api.go
+++ apis/falcon/v1alpha1/falcon_api.go
@@ -10,25 +10,17 @@ import (
 // ApiConfig generates standard gofalcon library api config
 func (fa *FalconAPI) ApiConfig() *falcon.ApiConfig {
 	return &falcon.ApiConfig{
-		Cloud:        fa.falconCloudInternal(),
+		Cloud:        falcon.Cloud(fa.CloudRegion),
 		ClientId:     fa.ClientId,
 		ClientSecret: fa.ClientSecret,
 	}
 }
 
 func (fa *FalconAPI) FalconCloud(ctx context.Context) (falcon.CloudType, error) {
-	cloud := fa.falconCloudInternal()
+	cloud := falcon.Cloud(fa.CloudRegion)
 	err := cloud.Autodiscover(ctx, fa.ClientId, fa.ClientSecret)
 	if err != nil {
 		return cloud, fmt.Errorf("Could not autodiscover Falcon Cloud Region. Please provide your cloud_region in FalconContainer Spec: %v", err)
 	}
 	return cloud, nil
-}
-
-func (fa *FalconAPI) falconCloudInternal() falcon.CloudType {
-	if fa.CloudRegion == "autodiscover" {
-		return falcon.Cloud("")
-	} else {
-		return falcon.Cloud(fa.CloudRegion)
-	}
 }
