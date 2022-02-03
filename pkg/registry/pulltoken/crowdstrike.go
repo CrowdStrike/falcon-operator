41cd1e0b22c9005e13d61435bcfa17df6154b7d5
Gabe Alford <redhatrises@gmail.com>
2022-02-03 16:40:45 -0700

    Ensure context is set for pulltoken

--- pkg/registry/pulltoken/crowdstrike.go
+++ pkg/registry/pulltoken/crowdstrike.go
@@ -1,13 +1,16 @@
 package pulltoken
 
 import (
+	"context"
+
 	"github.com/crowdstrike/falcon-operator/pkg/registry/falcon_registry"
 	"github.com/crowdstrike/gofalcon/falcon"
 )
 
 // CrowdStrike function returns kubernetes pull token for accessing CrowdStrike Falcon Registry.
 // Return value is in a form of corev1.SecretTypeDockerConfigJson (.dockerconfigjson)
-func CrowdStrike(apiConfig *falcon.ApiConfig) ([]byte, error) {
+func CrowdStrike(ctx context.Context, apiConfig *falcon.ApiConfig) ([]byte, error) {
+	apiConfig.Context = ctx
 	registry, err := falcon_registry.NewFalconRegistry(apiConfig)
 	if err != nil {
 		return nil, err
