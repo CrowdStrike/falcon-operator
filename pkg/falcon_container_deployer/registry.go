e15def0a8ca8aeee57ae612cb2b3ac622a6098d2
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-02-09 10:17:26 +0000

    Refactor: Drop unneeded function attribute
    CrowdStrike API is not needed to merge all the tokens.

--- pkg/falcon_container_deployer/registry.go
+++ pkg/falcon_container_deployer/registry.go
@@ -8,9 +8,7 @@ import (
 )
 
 func (d *FalconContainerDeployer) pulltokenBase64() (string, error) {
-	token, err := pulltoken.MergeAll(d.Ctx, d.Instance.Spec.Registry.Type, d.falconApiConfig(), d.Log,
-		k8s_utils.QuerySecrets(d.Namespace(), d.Client),
-	)
+	token, err := pulltoken.MergeAll(d.Ctx, d.Instance.Spec.Registry.Type, d.Log, k8s_utils.QuerySecrets(d.Namespace(), d.Client))
 	if err != nil || token == nil {
 		return "", err
 	}
--- pkg/registry/pulltoken/token.go
+++ pkg/registry/pulltoken/token.go
@@ -6,25 +6,15 @@ import (
 	falconv1alpha1 "github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1"
 	"github.com/crowdstrike/falcon-operator/pkg/k8s_utils"
 	"github.com/crowdstrike/falcon-operator/pkg/registry/auth"
-	"github.com/crowdstrike/gofalcon/falcon"
 	"github.com/go-logr/logr"
 )
 
-func MergeAll(ctx context.Context, registryType falconv1alpha1.RegistryTypeSpec, apiConfig *falcon.ApiConfig, log logr.Logger, query k8s_utils.KubeQuerySecretsMethod) ([]byte, error) {
+func MergeAll(ctx context.Context, registryType falconv1alpha1.RegistryTypeSpec, log logr.Logger, query k8s_utils.KubeQuerySecretsMethod) ([]byte, error) {
 	secrets, err := query(ctx)
 	if err != nil {
 		return nil, err
 	}
 	creds := auth.GetPullCredentials(secrets.Items)
-	/*
-		if registryType == falconv1alpha1.RegistryTypeCrowdStrike {
-			crwd, err := crowdStrikeCreds(apiConfig)
-			if err != nil {
-				return nil, err
-			}
-			creds = append(creds, crwd)
-		}
-	*/
 
 	return auth.MergeCredentials(creds, log)
 }
