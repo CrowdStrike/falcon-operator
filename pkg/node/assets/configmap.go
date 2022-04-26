ce5f0c65fc38b4dcf3981cee6d48e56fb87d5b7d
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-04-26 13:46:58 +0100

    Refactor: pass node.ConfigCache around instead of the CID

--- pkg/node/assets/configmap.go
+++ pkg/node/assets/configmap.go
@@ -3,12 +3,13 @@ package assets
 import (
 	falconv1alpha1 "github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1"
 	"github.com/crowdstrike/falcon-operator/pkg/common"
+	"github.com/crowdstrike/falcon-operator/pkg/node"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func DaemonsetConfigMap(dsname string, nsname string, cid string, falconsensor *falconv1alpha1.FalconSensor) *corev1.ConfigMap {
-	configData := common.FalconSensorConfig(falconsensor, cid)
+func DaemonsetConfigMap(dsname string, nsname string, config *node.ConfigCache, falconsensor *falconv1alpha1.FalconSensor) *corev1.ConfigMap {
+	configData := common.FalconSensorConfig(falconsensor, config.CID())
 
 	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
