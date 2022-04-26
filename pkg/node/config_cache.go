ded4e286b0bffd8868f052fb6ab6b87f04403890
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-04-26 10:28:35 +0100

    Do not crash when FalconAPI config is missing
    Addressing:
    panic: runtime error: invalid memory address or nil pointer dereference
    [signal SIGSEGV: segmentation violation code=0x1 addr=0x8 pc=0x1aa27fd]
    
    goroutine 329 [running]:
    github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1.(*FalconAPI).ApiConfig(0x0)
            /workspace/apis/falcon/v1alpha1/falcon_api.go:60 +0x1d
    github.com/crowdstrike/falcon-operator/pkg/node.NewConfigCache({0x2861980, 0xc000393350}, {{0xc000393350, 0x0}, 0x2881f38}, 0xc00014c1e0)
            /workspace/pkg/node/config_cache.go:21 +0x36
    github.com/crowdstrike/falcon-operator/controllers/falcon.(*FalconNodeSensorReconciler).Reconcile(0xc0006feae0, {0x2861980, 0xc000393350}, {{{0x0, 0x0}, {0xc00089ab70, 0x12}}})
            /workspace/controllers/falcon/falconnodesensor_controller.go:107 +0x3ab
    sigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).Reconcile(0xc00045f340, {0x2861980, 0xc000393230}, {{{0x0, 0x23c2400}, {0xc00089ab70, 0x413a94}}})
            /go/pkg/mod/sigs.k8s.io/controller-runtime@v0.11.0/pkg/internal/controller/controller.go:114 +0x26f
    sigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).reconcileHandler(0xc00045f340, {0x28618d8, 0xc0006e89c0}, {0x21efda0, 0xc00079ccc0})
            /go/pkg/mod/sigs.k8s.io/controller-runtime@v0.11.0/pkg/internal/controller/controller.go:311 +0x33e
    sigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).processNextWorkItem(0xc00045f340, {0x28618d8, 0xc0006e89c0})
            /go/pkg/mod/sigs.k8s.io/controller-runtime@v0.11.0/pkg/internal/controller/controller.go:266 +0x205
    sigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).Start.func2.2()
            /go/pkg/mod/sigs.k8s.io/controller-runtime@v0.11.0/pkg/internal/controller/controller.go:227 +0x85
    created by sigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).Start.func2
            /go/pkg/mod/sigs.k8s.io/controller-runtime@v0.11.0/pkg/internal/controller/controller.go:223 +0x357

--- pkg/node/config_cache.go
+++ pkg/node/config_cache.go
@@ -5,6 +5,7 @@ import (
 
 	falconv1alpha1 "github.com/crowdstrike/falcon-operator/apis/falcon/v1alpha1"
 	"github.com/crowdstrike/falcon-operator/pkg/falcon_api"
+	"github.com/crowdstrike/gofalcon/falcon"
 	"github.com/go-logr/logr"
 )
 
@@ -18,12 +19,23 @@ func (cc *ConfigCache) CID() string {
 }
 
 func NewConfigCache(ctx context.Context, logger logr.Logger, nodesensor *falconv1alpha1.FalconNodeSensor) (*ConfigCache, error) {
-	cid, err := falcon_api.FalconCID(ctx, nodesensor.Spec.Falcon.CID, nodesensor.Spec.FalconAPI.ApiConfig())
-	if err != nil {
-		return nil, err
+	var apiConfig *falcon.ApiConfig
+	var err error
+	cache := ConfigCache{}
+
+	if nodesensor.Spec.FalconAPI != nil {
+		apiConfig = nodesensor.Spec.FalconAPI.ApiConfig()
+		if nodesensor.Spec.FalconAPI.CID != nil {
+			cache.cid = *nodesensor.Spec.FalconAPI.CID
+		}
 	}
 
-	return &ConfigCache{
-		cid: cid,
-	}, nil
+	if cache.cid == "" {
+		cache.cid, err = falcon_api.FalconCID(ctx, nodesensor.Spec.Falcon.CID, apiConfig)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &cache, nil
 }
