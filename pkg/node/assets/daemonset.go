4feb281b0beb4537e370d9ebe463a44ef0f6b588
Gabe Alford <redhatrises@gmail.com>
2022-06-21 19:04:50 -0600

    If DS update strategy is empty, default to RollingUpdate

--- pkg/node/assets/daemonset.go
+++ pkg/node/assets/daemonset.go
@@ -31,19 +31,19 @@ func pullSecrets(node *falconv1alpha1.FalconNodeSensor) []corev1.LocalObjectRefe
 }
 
 func dsUpdateStrategy(node *falconv1alpha1.FalconNodeSensor) appsv1.DaemonSetUpdateStrategy {
-	rollingUpdateSettings := appsv1.RollingUpdateDaemonSet{}
+	if node.Spec.Node.DSUpdateStrategy.Type == appsv1.RollingUpdateDaemonSetStrategyType || node.Spec.Node.DSUpdateStrategy.Type == "" {
+		rollingUpdateSettings := appsv1.RollingUpdateDaemonSet{}
 
-	/* Beta feature to enable later
-	if node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxSurge != nil {
-		rollingUpdateSettings.MaxSurge = node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxSurge
-	}
-	*/
+		/* Beta feature to enable later
+		if node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxSurge != nil {
+			rollingUpdateSettings.MaxSurge = node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxSurge
+		}
+		*/
 
-	if node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxUnavailable != nil {
-		rollingUpdateSettings.MaxUnavailable = node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxUnavailable
-	}
+		if node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxUnavailable != nil {
+			rollingUpdateSettings.MaxUnavailable = node.Spec.Node.DSUpdateStrategy.RollingUpdate.MaxUnavailable
+		}
 
-	if node.Spec.Node.DSUpdateStrategy.Type == appsv1.RollingUpdateDaemonSetStrategyType {
 		return appsv1.DaemonSetUpdateStrategy{
 			Type:          appsv1.RollingUpdateDaemonSetStrategyType,
 			RollingUpdate: &rollingUpdateSettings,
