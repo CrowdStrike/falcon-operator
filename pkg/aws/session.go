42a53733d825cb73ddfb558a09cc92979fb0a03b
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-06-28 19:03:11 +0100

    Remove the code that has been commented-out

--- pkg/aws/session.go
+++ pkg/aws/session.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	//"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
-	//"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 type Config struct {
