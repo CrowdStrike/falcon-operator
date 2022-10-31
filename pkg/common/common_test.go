5a9f404a7d9d11fa5b7da060a4eda6ff71e26d61
Gabe Alford <redhatrises@gmail.com>
2022-10-31 13:24:21 -0600

    tests: add version and common tests

---
+++ pkg/common/common_test.go
@@ -0,0 +1,50 @@
+package common
+
+import (
+	"reflect"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/version"
+	ktest "k8s.io/client-go/testing"
+)
+
+type FakeDiscovery struct {
+	*ktest.Fake
+	FakedServerVersion *version.Info
+}
+
+func TestInitContainerArgs(t *testing.T) {
+	want := []string{"-c", "mkdir -p /opt/CrowdStrike && touch /opt/CrowdStrike/falconstore"}
+	if got := InitContainerArgs(); !reflect.DeepEqual(got, want) {
+		t.Errorf("InitContainerArgs() = %v, want %v", got, want)
+	}
+}
+
+func TestInitCleanupArgs(t *testing.T) {
+	want := []string{"-c", "rm -rf /opt/CrowdStrike"}
+	if got := InitCleanupArgs(); !reflect.DeepEqual(got, want) {
+		t.Errorf("InitCleanupArgs() = %v, want %v", got, want)
+	}
+}
+
+func TestCleanupSleep(t *testing.T) {
+	want := []string{"-c", "sleep 10"}
+	if got := CleanupSleep(); !reflect.DeepEqual(got, want) {
+		t.Errorf("CleanupSleep() = %v, want %v", got, want)
+	}
+}
+
+func TestEncodedBase64String(t *testing.T) {
+	want := "dGVzdA=="
+	if got := string(EncodedBase64String("test")); !reflect.DeepEqual(got, want) {
+		t.Errorf("EncodedBase64String() = %v, want %v", got, want)
+	}
+}
+
+func TestCleanDecodedBase64(t *testing.T) {
+	want := []byte("test")
+
+	if got := CleanDecodedBase64(EncodedBase64String("\t\ntest\n\t")); !reflect.DeepEqual(got, want) {
+		t.Errorf("CleanDecodedBase64() = %v, want %v", got, want)
+	}
+}
