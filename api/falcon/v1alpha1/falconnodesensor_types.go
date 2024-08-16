ae9c8fbeda2cb4e564e6228ea08108ebbbd11164
Milo Hyson <milo.hyson@crowdstrike.com>
2024-08-16 15:28:34 -0700

    fix: corrected display names in unsafe sub-specs

--- api/falcon/v1alpha1/falconnodesensor_types.go
+++ api/falcon/v1alpha1/falconnodesensor_types.go
@@ -107,7 +107,7 @@ type FalconNodeSensorConfig struct {
 	// FalconUnsafe configures various options that go against industry practices or are otherwise not recommended for use.
 	// Adjusting these settings may result in incorrect or undesirable behavior. Proceed at your own risk.
 	// For more information, please see https://github.com/CrowdStrike/falcon-operator/blob/main/UNSAFE.md.
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Falcon Admission Controller Unsafe Settings"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="DaemonSet Unsafe Settings"
 	Unsafe FalconUnsafe `json:"unsafe,omitempty"`
 }
 
--- api/falcon/v1alpha1/unsafe.go
+++ api/falcon/v1alpha1/unsafe.go
@@ -5,6 +5,6 @@ package v1alpha1
 // For more information, please see https://github.com/CrowdStrike/falcon-operator/blob/main/UNSAFE.md.
 type FalconUnsafe struct {
 	// UpdatePolicy is the name of a sensor update policy configured and enabled in Falcon UI. It is ignored when Image and/or Version are set.
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Falcon Admission Controller Update Policy",order=1
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Falcon Sensor Update Policy",order=1
 	UpdatePolicy *string `json:"updatePolicy,omitempty"`
 }
