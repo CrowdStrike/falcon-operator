5af863e587e0631de82e975b7d80a08661304f24
Jung Choi <jung.choi@crowdstrike.com>
2025-04-25 15:07:37 -0400

    add falcon secret to node sensor and kac custom resources

--- api/falcon/v1alpha1/falconadmission_types.go
+++ api/falcon/v1alpha1/falconadmission_types.go
@@ -45,6 +45,15 @@ type FalconAdmissionSpec struct {
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Falcon Platform API Configuration",order=2
 	FalconAPI *FalconAPI `json:"falcon_api,omitempty"`
 
+	// FalconSecret config if used to inject k8s secrets with sensitive data for the Falcon sensor and the Falcon API.
+	// The following Falcon values are supported by k8s secret injection:
+	//   FalconCID
+	//   FalconProvisioningToken
+	//   FalconClientId
+	//   FalconClientSecret
+	// +kubebuilder:default={}
+	FalconSecret FalconSecret `json:"falconSecret"`
+
 	// ResourceQuota configures the ResourceQuota for the Falcon Admission Controller. This is useful for limiting the number of pods that can be created in the namespace.
 	// +kubebuilder:default:={}
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Falcon Admission Controller Resource Quota",order=4
