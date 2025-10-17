516f0cf1226c80dc424cd89936b44991a4c898a1
Ivan Aguilar <ivan.aguilar.cortes@ookla.com>
2025-10-17 14:34:43 +0200

    reconciler checks

--- test/e2e/tolerations_test.go
+++ test/e2e/tolerations_test.go
@@ -15,8 +15,8 @@ import (
 	//nolint:revive
 	. "github.com/onsi/gomega"
 
-	corev1 "k8s.io/api/core/v1"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // Toleration represents the expected toleration to validate
