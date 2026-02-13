531f02a61e432c440654a9836cd9a1a481216a65
Greg Pontejos <242696964+gpontejos-cs@users.noreply.github.com>
2026-02-13 14:36:48 -0600

    feat(falcon-iar): Add unified IAR sensor support

--- pkg/registry/falcon_registry/container.go
+++ pkg/registry/falcon_registry/container.go
@@ -2,9 +2,11 @@ package falcon_registry
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/crowdstrike/gofalcon/falcon"
+	"go.podman.io/image/v5/types"
 	"golang.org/x/mod/semver"
 )
 
@@ -12,6 +14,7 @@ const (
 	MinimumUnifiedNodeSensorVersion    = "7.31.0"
 	MinimumUnifiedKacSensorVersion     = "7.33.0"
 	MinimumUnifiedSidecarSensorVersion = "7.33.0"
+	MinimumUnifiedImageSensorVersion   = "1.0.24"
 )
 
 func (reg *FalconRegistry) LastContainerTag(ctx context.Context, sensorType falcon.SensorType, versionRequested *string) (string, error) {
@@ -24,7 +27,7 @@ func (reg *FalconRegistry) LastContainerTag(ctx context.Context, sensorType falc
 
 	regionedFilter := func(tag string) bool {
 		tagContains := ".container"
-		if sensorType == falcon.RegionedImageSensor || sensorType == falcon.KacSensor {
+		if sensorType == falcon.ImageSensor || sensorType == falcon.KacSensor {
 			tagContains = ""
 		}
 
@@ -40,15 +43,23 @@ func (reg *FalconRegistry) LastContainerTag(ctx context.Context, sensorType falc
 
 	switch sensorType {
 	case falcon.KacSensor:
-		tag, err = lastTag(ctx, systemContext, falcon.FalconContainerSensorImageURI(reg.falconCloud, falcon.KacSensor), unifiedFilter)
-		if err != nil {
-			tag, err = lastTag(ctx, systemContext, falcon.FalconContainerSensorImageURI(reg.falconCloud, falcon.RegionedKacSensor), regionedFilter)
-		}
+		tag, err = reg.tryUnifiedThenRegioned(
+			ctx, systemContext,
+			falcon.KacSensor, falcon.RegionedKacSensor,
+			unifiedFilter, regionedFilter,
+		)
 	case falcon.SidecarSensor:
-		tag, err = lastTag(ctx, systemContext, falcon.FalconContainerSensorImageURI(reg.falconCloud, falcon.SidecarSensor), unifiedFilter)
-		if err != nil {
-			tag, err = lastTag(ctx, systemContext, falcon.FalconContainerSensorImageURI(reg.falconCloud, falcon.RegionedSidecarSensor), regionedFilter)
-		}
+		tag, err = reg.tryUnifiedThenRegioned(
+			ctx, systemContext,
+			falcon.SidecarSensor, falcon.RegionedSidecarSensor,
+			unifiedFilter, regionedFilter,
+		)
+	case falcon.ImageSensor:
+		tag, err = reg.tryUnifiedThenRegioned(
+			ctx, systemContext,
+			falcon.ImageSensor, falcon.RegionedImageSensor,
+			unifiedFilter, regionedFilter,
+		)
 	default:
 		tag, err = lastTag(ctx, systemContext, reg.imageUriContainer(sensorType), regionedFilter)
 	}
@@ -68,7 +79,29 @@ func IsMinimumUnifiedSensorVersion(version string, sensorType falcon.SensorType)
 		return semver.Compare("v"+version, "v"+MinimumUnifiedKacSensorVersion) >= 0
 	case falcon.SidecarSensor:
 		return semver.Compare("v"+version, "v"+MinimumUnifiedSidecarSensorVersion) >= 0
+	case falcon.ImageSensor:
+		return semver.Compare("v"+version, "v"+MinimumUnifiedImageSensorVersion) >= 0
 	}
 
 	return false
+}
+
+func (reg *FalconRegistry) tryUnifiedThenRegioned(
+	ctx context.Context,
+	systemContext *types.SystemContext,
+	unifiedType, regionedType falcon.SensorType,
+	unifiedFilter, regionedFilter func(string) bool,
+) (string, error) {
+	unifiedURI := falcon.FalconContainerSensorImageURI(reg.falconCloud, unifiedType)
+	regionedURI := falcon.FalconContainerSensorImageURI(reg.falconCloud, regionedType)
+
+	tag, err := lastTag(ctx, systemContext, unifiedURI, unifiedFilter)
+	if err != nil {
+		unifiedErr := fmt.Errorf("failed to fetch unified image sensor tag: %w", err)
+		tag, err = lastTag(ctx, systemContext, regionedURI, regionedFilter)
+		if err != nil {
+			return "", fmt.Errorf("failed to fetch regioned image sensor tag: %w; previous error: %v", err, unifiedErr)
+		}
+	}
+	return tag, nil
 }
