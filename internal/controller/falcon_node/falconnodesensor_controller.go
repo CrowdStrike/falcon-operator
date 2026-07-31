edb32cd12f2a302af69b800c531dc35ae1269cce
Jung Choi <17916440+mr-jungchoi@users.noreply.github.com>
2026-07-31 12:01:51 -0400

    update node.backend docs to align with sensor docs (#844)

--- internal/controller/falcon_node/falconnodesensor_controller.go
+++ internal/controller/falcon_node/falconnodesensor_controller.go
@@ -119,7 +119,7 @@ func (r *FalconNodeSensorReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 
 	if nodesensor.Spec.Node.Backend != "" {
-		logger.V(1).Info("FalconNodeSensor backend field is deprecated and will be ignored; the backend is always bpf", "backend", nodesensor.Spec.Node.Backend)
+		logger.V(1).Info("FalconNodeSensor backend field is deprecated and will be ignored; It may be removed in a future sensor release.", "backend", nodesensor.Spec.Node.Backend)
 	}
 
 	validate, err := k8sutils.CheckRunningPodLabels(r.Reader, ctx, nodesensor.Spec.InstallNamespace, common.CRLabels("daemonset", nodesensor.Name, common.FalconKernelSensor))
