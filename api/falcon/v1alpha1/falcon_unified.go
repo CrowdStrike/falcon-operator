4243a02a5d328551045ff6463e393d23cd85c01b
Engineering Systems <techops-cicd@crowdstrike.com>
2026-08-10 21:38:43 +0000

    chore: add us-3 (#839)

--- api/falcon/v1alpha1/falcon_unified.go
+++ api/falcon/v1alpha1/falcon_unified.go
@@ -5,7 +5,7 @@ type FalconUnified struct {
 	FalconSensor `json:",inline"`
 
 	// Falcon Customer Cloud Region - With the unified installer, you can let the sensor discover the CID's cloud automatically, or you can specify the cloud where the CID resides.
-	// +kubebuilder:validation:Enum=us-1;us-2;eu-1;us-gov-1;us-gov-2
+	// +kubebuilder:validation:Enum=us-1;us-2;us-3;eu-1;us-gov-1;us-gov-2
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="CrowdStrike Falcon Cloud Region"
 	Cloud string `json:"cloud,omitempty"`
 }
